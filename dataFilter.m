be48269abde0a413d7b0388f58f3db1e78bbbdae
Mads Frimann Madsen <mfrimannm@gmail.com>
2015-01-15 17:43:42 +0100

    mangler kun kommentarer

--- dataFilter.m
+++ dataFilter.m
@@ -1,79 +1,66 @@
 function newData = dataFilter(data, filterChoice)
-    % data is a matix with data type per column, like temp - growth - type
-    % filterChoice is a vector N per 1, it will be composed so this
-    % function can make its action so fist a bit saying 1/0 if the action
-    % should be done, and then how it should be done.
-    
-    % We make a vector that decomposes the filterChoice vector to be used
-    % through the function.
-    filterChoice = num2cell(filterChoice);
-    [filterBacteria,bacteria1,bacteria2,bacteria3,bacteria4,filterTemp,minTemp,maxTemp,filterGrowthRate,minGrowthRate,maxGrowthRate] = deal(filterChoice{:});
-    
-    % vælg hvilke bakterier, 1 ,2 , 3, 4
-    % filterBak 1/0
-    % tilvælg bak 1,2,3,4
-    
-    if filterBacteria
-        if not(bacteria1)
-            data = data((data(:,3)~=1),:);
-        end
-        
-        if not(bacteria2)
-            data = data((data(:,3)~=2),:);
-        end
-        
-        if not(bacteria3)
-            data = data((data(:,3)~=3),:);
-        end
-        
-        if not(bacteria4)
-            data = data((data(:,3)~=4),:);
-        end
-        
-    end
-       
-    % Vælge inden for maxs min temp, hvis en ikke sættes så standart.
-    % filterTemp 1/0
-    % hvis 0 er givet ved min/max sættes standart
-    % min 10 eller over
-    % max 60 eller under
-    
-    if filterTemp
-        if minTemp == 0
-            minTemp = 10;
-        end
-        if maxTemp == 0
-            maxTemp = 60;
-        end
-        if minTemp > maxTemp
-            temp = minTemp;
-            minTemp = maxTemp;
-            maxTemp = temp;
-        end
-        
-        data = data((data(:,1)> minTemp),:);
-        data = data((data(:,1)< maxTemp),:);
+% data is a matix with data type per column, like temp - growth - type
+% filterChoice is a vector N per 1, it will be composed so this
+% function can make its action so fist a bit saying 1/0 if the action
+% should be done, and then how it should be done.
+
+% this does so the data input does not get chanced
+newData = data;
+
+% We make a vector that decomposes the filterChoice vector to be used
+% through the function.
+filterChoice = num2cell(filterChoice);
+[filterBacteria,bacteria1,bacteria2,bacteria3,bacteria4,filterTemp,minTemp,maxTemp,filterGrowthRate,minGrowthRate,maxGrowthRate] = deal(filterChoice{:});
+
+% Bacteria
+if filterBacteria
+    if not(bacteria1)
+        newData = newData((newData(:,3)~=1),:);
     end
     
-    % vælge inden for maxs min Groth speed, hvis en ikke sættes så standart.
-    % filterGrowthRate 1/0
-    % hvis -1 er givet ved min/max sættes standart
-    % min 0 eller over
-    % max uendelig eller under
-    
-    if filterGrowthRate
-        if minGrowthRate > maxGrowthRate
-            temp = minGrowthRate;
-            minGrowthRate = maxGrowthRate;
-            maxGrowthRate = temp;
-        end
-        
-        data = data((data(:,2)> minGrowthRate),:);
-        
-        if maxGrowthRate > 0
-        data = data((data(:,2)< maxGrowthRate),:);
-        end
+    if not(bacteria2)
+        newData = newData((newData(:,3)~=2),:);
     end
     
-    newData = data;
-end 
+    if not(bacteria3)
+        newData = newData((newData(:,3)~=3),:);
+    end
+    
+    if not(bacteria4)
+        newData = newData((newData(:,3)~=4),:);
+    end
+    
+end
+
+% Temp
+if filterTemp
+    if minTemp == 0
+        minTemp = 10;
+    end
+    if maxTemp == 0
+        maxTemp = 60;
+    end
+    if minTemp > maxTemp
+        temp = minTemp;
+        minTemp = maxTemp;
+        maxTemp = temp;
+    end
+    
+    newData = newData((newData(:,1)> minTemp),:);
+    newData = newData((newData(:,1)< maxTemp),:);
+end
+% GrowthRate
+if filterGrowthRate
+    if minGrowthRate > maxGrowthRate
+        temp = minGrowthRate;
+        minGrowthRate = maxGrowthRate;
+        maxGrowthRate = temp;
+    end
+    
+    newData = newData((newData(:,2)> minGrowthRate),:);
+    
+    if maxGrowthRate > 0
+        newData = newData((newData(:,2)< maxGrowthRate),:);
+    end
+end
+end
--- filterInput.m
+++ filterInput.m
@@ -1,99 +1,95 @@
 function filterChoice = filterInput
 % Denne funktion prompter brugeren for input der bruges til filtreringen.
-    filterChoice = zeros(1,11);
+% Disse instillinger gemmen i en vector som bruges andet sted i programmet.
+filterChoice = zeros(1,11);
+
+% Bacteria
+bacYN = lower(input('\nWould you like to filter with respect to bacteria types? (Y/N): ', 's'));
+if bacYN == 'y'
+    filterChoice(1) = 1;
+    bac_1YN = lower(input('\nWould you like to include Salmonella enterica in the data? (Y/N): ', 's'));
+    if bac_1YN == 'y'
+        filterChoice(2) = 1;
+    end
     
-    bacYN = lower(input('\nWould you like to filter with respect to bacteria types? (Y/N): ', 's'));
-    if bacYN == 'y'
-        filterChoice(1) = 1;
-        bac_1YN = lower(input('\nWould you like to include Salmonella enterica in the data? (Y/N): ', 's'));
-        if bac_1YN == 'y'
-            filterChoice(2) = 1;
-        end
-
-        bac_2YN = lower(input('\nWould you like to include Bacillus cereus in the data? (Y/N): ', 's'));
-        if bac_2YN == 'y'
-            filterChoice(3) = 1;
-        end
-
-        bac_3YN = lower(input('\nWould you like to include Listeria in the data? (Y/N): ', 's'));
-        if bac_3YN == 'y'
-            filterChoice(4) = 1;
-        end
-
-        bac_4YN = lower(input('\nWould you like to include Brochothrix thermosphacta in the data? (Y/N): ', 's'));
-        if bac_4YN == 'y'
-            filterChoice(5) = 1;
-        end
+    bac_2YN = lower(input('\nWould you like to include Bacillus cereus in the data? (Y/N): ', 's'));
+    if bac_2YN == 'y'
+        filterChoice(3) = 1;
     end
+    
+    bac_3YN = lower(input('\nWould you like to include Listeria in the data? (Y/N): ', 's'));
+    if bac_3YN == 'y'
+        filterChoice(4) = 1;
+    end
+    
+    bac_4YN = lower(input('\nWould you like to include Brochothrix thermosphacta in the data? (Y/N): ', 's'));
+    if bac_4YN == 'y'
+        filterChoice(5) = 1;
+    end
+end
 
-    tempYN = lower(input('\nWould you like to filter with respect to temperature? (Y/N): ', 's'));
-    if tempYN == 'y'
-        filterChoice(6) = 1;
-        tempMinYN = lower(input('\nWould you like to input a minimum temperature for the filter? (Y/N): ', 's'));
-        if tempMinYN == 'y'
-            run = 1;
-            while run
-                minTemp = str2double(input('\nPlease insert the desired limit: ', 's'));
-                if isnan(minTemp)
-                    fprintf('\nInvalid number, please input valid number');
-                else
-                    minTemp = str2double(minTemp);
-                    filterChoice(7) = minTemp;
-                    run = 0;
-                end
-            end
-        end
-        tempMaxYN = lower(input('\nWould you like to input a maximum temperature for the filter? (Y/N): ', 's'));
-        if tempMaxYN == 'y'
-            run = 1;
-            while run
-                maxTemp = str2double(input('\nPlease insert the desired limit', 's'));
-                if isnan(maxTemp)
-                    fprintf('\nInvalid number, please input valid number');
-                else
-                    maxTemp = str2double(maxTemp);
-                    filterChoice(8) = maxTemp;
-                    run = 0;
-                end
+% Temp
+tempYN = lower(input('\nWould you like to filter with respect to temperature? (Y/N): ', 's'));
+if tempYN == 'y'
+    filterChoice(6) = 1;
+    tempMinYN = lower(input('\nWould you like to input a minimum temperature for the filter? (Y/N): ', 's'));
+    if tempMinYN == 'y'
+        run = 1;
+        while run
+            minTemp = str2double(input('\nPlease insert the desired limit: ', 's'));
+            if isnan(minTemp)
+                fprintf('\nInvalid number, please input valid number: ');
+            else
+                filterChoice(7) = minTemp;
+                run = 0;
             end
         end
     end
+    tempMaxYN = lower(input('\nWould you like to input a maximum temperature for the filter? (Y/N): ', 's'));
+    if tempMaxYN == 'y'
+        run = 1;
+        while run
+            maxTemp = str2double(input('\nPlease insert the desired limit: ', 's'));
+            if isnan(maxTemp)
+                fprintf('\nInvalid number, please input valid number: ');
+            else
+                filterChoice(8) = maxTemp;
+                run = 0;
+            end
+        end
+    end
+end
 
-    growthYN = lower(input('\nWould you like to filter with respect to growth rate? (Y/N): ', 's'));
-    if growthYN == 'y'
-        filterChoice(9) = 1;
-        growthMinYN = lower(input('\nWould you like to input a minimum growth rate for the filter? (Y/N): ', 's'));
-        if growthMinYN == 'y'
-            run = 1;
-            while run
-                minGrowth = str2double(input('\nPlease insert the desired limit: ', 's'));
-                if isnan(minGrowth)
-                    fprintf('\nInvalid number, please input valid number');
-                else
-                    minGrowth = str2double(minGrowth);
-                    filterChoice(8) = minGrowth;
-                    run = 0;
-                end
+% GrowthRate
+growthYN = lower(input('\nWould you like to filter with respect to growth rate? (Y/N): ', 's'));
+if growthYN == 'y'
+    filterChoice(9) = 1;
+    growthMinYN = lower(input('\nWould you like to input a minimum growth rate for the filter? (Y/N): ', 's'));
+    if growthMinYN == 'y'
+        run = 1;
+        while run
+            minGrowth = str2double(input('\nPlease insert the desired limit: ', 's'));
+            if isnan(minGrowth)
+                fprintf('\nInvalid number, please input valid number');
+            else
                 filterChoice(10) = minGrowth;
+                run = 0;
             end
-        end
-        growthMaxYN = lower(input('\nWould you like to input a maximum growth rate for the filter? (Y/N): ', 's'));
-        if growthMaxYN == 'y'
-            run = 1;
-            while run
-                maxGrowth = str2double(input('\nPlease insert the desired limit: ', 's'));
-                if isnan(maxGrowth)
-                    fprintf('\nInvalid number, please input valid number');
-                else
-                    maxGrowth = str2double(maxGrowth);
-                    filterChoice(8) = maxGrowth;
-                    run = 0;
-                end
-                filterChoice(10) = minGrowth;
-            end
-            filterChoice(11) = maxGrowth;
         end
     end
-
+    growthMaxYN = lower(input('\nWould you like to input a maximum growth rate for the filter? (Y/N): ', 's'));
+    if growthMaxYN == 'y'
+        run = 1;
+        while run
+            maxGrowth = str2double(input('\nPlease insert the desired limit: ', 's'));
+            if isnan(maxGrowth)
+                fprintf('\nInvalid number, please input valid number');
+            else
+                filterChoice(11) = maxGrowth;
+                run = 0;
+            end
+        end
+    end
+end
 end
 
