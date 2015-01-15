1bd0de968317400a88a5c196658b4eb650218209
Mads Frimann Madsen <mfrimannm@gmail.com>
2015-01-15 11:59:07 +0100

    new filter function

---
+++ filterData.m
@@ -0,0 +1,9 @@
+function newData = filterData(data, filterChoice)
+    % data is a matix with data type per clummen, like temp - growth - type
+    % filterChoice is a vector N per 1, it will be composed so this
+    % function kan make its action so fist a bit sayingen 1/0 if the action
+    % should be done, and then how it should be done.
+    
+    % i make a vector that decompeses the filterChoice vector to be used
+    % through the function.
+    
