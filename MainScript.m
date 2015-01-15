ff882df0b9c219706617d7816482381b7163fc39
Mads Frimann Madsen <mfrimannm@gmail.com>
2015-01-15 16:12:54 +0100

    clean up menu, removed all while true

--- MainScript.m
+++ MainScript.m
@@ -1,50 +1,53 @@
-
 loaded = 0;
+runProgram = 1;
+runMenu = 0;
 
-while true
-    fprintf('\n 1. Load data.\n 2. Filter data.\n 3. Show statistic.\n 4. Generate diagrams.\n 5. Cancel.\n\n');
+while runProgram
+    fprintf('\n^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^\n 1. Load data.\n 2. Filter data.\n 3. Show statistic.\n 4. Generate diagrams.\n 5. Cancel.\n\n');
     menuChoice = str2double(input('Please enter the number of the action of choice: ', 's'));
     switch (menuChoice + loaded)
-        case 1  
-            while true
+        case 1 % load datafile
+            runMenu = 1;
+            while runMenu
             fprintf('\nYou chose to load data.\nTo cancel enter "cancel"\n');  
             filename = input('Please enter the name of the file you want to load: ', 's');
                 if strcmpi(filename,'cancel')
-                    break;
+                    runMenu = 0;
                 elseif exist(filename,'file') ~= 0
                     data = dataLoad(filename);
                     newData = data;
+                    fprintf('\nYou have successfully loaded the datafile named: %s\n',filename); 
                     loaded = 10;
-                    break;
+                    runMenu = 0;
                 else
                    fprintf('\nInvalid filename, please try again.\n');
                    clear filename;
                 end
             end                 
-        case 12
+        case 12 % Filter data
             fprintf('\nYou chose to filter data.\nTo cancel enter "cancel"\n');
             newData = filterData(data,filterChoice);
-            break;
-        case 13
-            while true
+        case 13 % Show statistic
+            runMenu = 1;
+            while runMenu
                 fprintf('\nYou chose to show statistic.\nYou have the following filter options:\n');
                 fprintf('\n 1. Mean Temperature\n 2. Mean Growth rate\n 3. Std Temperature\n 4. Std Growth rate\n 5. Rows\n 6. Mean Cold Growth rate\n 7. Mean Hot Growth rate\n 8. Cancel\n\n');
                 statistic = input('Please enter your choice of action: ', 's');
                 if strcmpi(statistic,'cancel') || strcmp(statistic,'8')
                     fprintf('\nYou chose to cancel.\n');
-                    break;
+                    runMenu = 0;
                 else
                    result = dataStatistics(newData, statistic);
                    fprintf('You chose to show %s which is %d \n',statistic,result);
-                   break;
+                   runMenu = 0;
                 end
             end
-        case 14
+        case 14 % Generate diagrams
             fprintf('\nYou chose to generate diagrams.\n');
             dataPlot(newData)
-        case {5, 15}
+        case {5, 15} % quit program
             fprintf('\nYou chose to cancel.\n');
-            break;
+            runProgram = 0;
         otherwise
             fprintf('\nInvalid action. Please try again.\nRemember to load data first.\n')
     end
