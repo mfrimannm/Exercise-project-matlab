69eacbb5659281c6b439981d43e5b9b828b3078b
TheLaks <s134089@student.dtu.dk>
2015-01-15 14:54:21 +0100

    Funktion til at rette output til statistic
    Modtager den string der skal printes, og sikrer at den er pæn.

---
+++ changeStatString.m
@@ -0,0 +1,25 @@
+function newString = changeStatString(statString)
+% En switch der ændrer den string der skal printes i main, for at give et
+% pænere output.
+
+switch statString
+    case 1
+        newString = 'Mean Temperature';
+    case 2
+        newString = 'Mean Growth rate';
+    case 3
+        newString = 'Std Temperature';
+    case 4
+        newString = 'Std Growth rate';
+    case 5
+        newString = 'Rows';
+    case 6
+        newString = 'Mean Cold Growth rate';
+    case 7
+        newString = 'Mean Hot Growth rate';
+    otherwise    
+        newString = statString;
+end
+
+end
+
