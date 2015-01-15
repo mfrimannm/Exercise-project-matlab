e12fdceaf769535b7313d3e92a6027e05cb36e34
Michael Jacobsen <leksu7@hotmail.com>
2015-01-15 14:55:27 +0100

    MainScript, major changes
    - Every case now works with the respected functions.
    - Show statistics now actually shows the statistics! :dart:
    - The menu system has been optimazed for better looks and
    functionallity.

--- MainScript.m
+++ MainScript.m
@@ -13,6 +13,7 @@ while true
                     break;
                 elseif exist(filename,'file') ~= 0
                     data = dataLoad(filename);
+                    newData = data;
                     loaded = 10;
                     break;
                 else
@@ -22,17 +23,25 @@ while true
             end                 
         case 12
             fprintf('\nYou chose to filter data.\nTo cancel enter "cancel"\n');
-            %dataFilter fuction?!?!?
+            newData = filterData(data,filterChoice);
             break;
         case 13
-            fprintf('\nYou chose to show statistic.\nTo cancel enter "cancel"\n');
-            statistic = input('Please enter the name of the file you want to load: ', 's');
-            dataStatistics(data, statistic)
-            break;
+            while true
+                fprintf('\nYou chose to show statistic.\nYou have the following filter options:\n');
+                fprintf('\n 1. Mean Temperature\n 2. Mean Growth rate\n 3. Std Temperature\n 4. Std Growth rate\n 5. Rows\n 6. Mean Cold Growth rate\n 7. Mean Hot Growth rate\n 8. Cancel\n\n');
+                statistic = input('Please enter your choice of action: ', 's');
+                if strcmpi(statistic,'cancel') || strcmp(statistic,'8')
+                    fprintf('\nYou chose to cancel.\n');
+                    break;
+                else
+                   result = dataStatistics(newData, statistic);
+                   fprintf('You chose to show %s which is %d \n',statistic,result);
+                   break;
+                end
+            end
         case 14
-            fprintf('\nYou chose to generate diagrams\n');
-            dataPlot(data)
-            %break;
+            fprintf('\nYou chose to generate diagrams.\n');
+            dataPlot(newData)
         case {5, 15}
             fprintf('\nYou chose to cancel.\n');
             break;
