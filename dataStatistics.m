5161d651134f734367313a06f69608fe1b6c0000
Mads Frimann Madsen <mfrimannm@gmail.com>
2015-01-16 04:54:22 +0100

    gjort lidt pænere

--- dataStatistics.m
+++ dataStatistics.m
@@ -1,32 +1,32 @@
 function result = dataStatistics(data, statistic)
-    % Denne funktion modtager data samt hvilken type statistik der ønskes,
-    % og returnerer så statistikken.
-    
-    statistic = lower(statistic);
-    
-    % i denne switch vælges der generalt først data, og derefter berenges
-    % det ønskede.
-    switch statistic
-        case {'mean temperature','1'}
-            result = mean(data(:,1));
-        case {'mean growth rate','2'}
-            result = mean(data(:,2));
-        case {'std temperature','3'}
-            result = std(data(:,1));
-        case {'std growth rate','4'}
-            result = std(data(:,2));
-        case {'rows','5'}
-            temp = size(data);
-            result = temp(1);
-        case {'mean cold rowth rate','6'}
-            temp = data(:,1:2);
-            temp = temp(temp(:,1) < 20,:);                       
-            result = mean(temp(:,2));
-        case {'mean hot growth rate','7'}
-            temp = data(:,1:2);
-            temp = temp(temp(:,1) > 50,:);                    
-            result = mean(temp(:,2));
-        otherwise
-            result = 'Wrong input';
-    end
+% Denne funktion modtager data samt hvilken type statistik der ønskes,
+% og returnerer så statistikken.
+
+statistic = lower(statistic);
+
+% i denne switch vælges der generalt først data, og derefter berenges
+% det ønskede.
+switch statistic
+    case {'mean temperature','1'}
+        result = mean(data(:,1));
+    case {'mean growth rate','2'}
+        result = mean(data(:,2));
+    case {'std temperature','3'}
+        result = std(data(:,1));
+    case {'std growth rate','4'}
+        result = std(data(:,2));
+    case {'rows','5'}
+        temp = size(data);
+        result = temp(1);
+    case {'mean cold rowth rate','6'}
+        temp = data(:,1:2);
+        temp = temp(temp(:,1) < 20,:);
+        result = mean(temp(:,2));
+    case {'mean hot growth rate','7'}
+        temp = data(:,1:2);
+        temp = temp(temp(:,1) > 50,:);
+        result = mean(temp(:,2));
+    otherwise
+        result = 'Wrong input';
+end
 end
