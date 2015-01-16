c15bb17ae4ddfd7b970e8f1dbb653a8465ade14b
Mads Frimann Madsen <mfrimannm@gmail.com>
2015-01-16 04:53:04 +0100

    Seltet disse to filer

--- testafstatetisk.m
+++
@@ -1,8 +0,0 @@
-load('testFile1.dat');
-dataStatistics(testFile1,'Mean Temperature')
-dataStatistics(testFile1,'Mean Growth rate')
-dataStatistics(testFile1,'Std Temperature')
-dataStatistics(testFile1,'Std Growth rate')
-dataStatistics(testFile1,'Rows')
-dataStatistics(testFile1,'Mean Cold Growth rate')
-dataStatistics(testFile1,'Mean Hot Growth rate')
