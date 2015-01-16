7639680a57f5679af4e5961a4ea702fd575940d7
Mads Frimann Madsen <mfrimannm@gmail.com>
2015-01-16 11:08:06 +0100

    næsten færdig

--- dataLoad.m
+++ dataLoad.m
@@ -4,6 +4,28 @@ file = load(filename);
 N = length(file'); %N equals number of rows in filename, i.e. length of filename transposed.
 data = zeros(N, 3); %initializes data matrix for speed optimization.
 
+count = 0;
+
+for i = 1:N
+    if (file(i, 1) >= 10) && (file(i, 1) <= 60)
+        if file(i, 2) >= 0
+            if (file(i, 3) >= 1) && (file(i, 3) <= 4)
+                data(i, :) = file(i, :);
+                count = count + 1;
+            else
+                fprintf('\nThe bacteira in row %d did not meet the condtions and was removed.\n',i);
+            end
+        else
+            fprintf('\nThe growth rate in row %d did not meet the condtions and was removed.\n',i);
+        end
+    else
+        fprintf('\nThe temperature in row %d did not meet the condtions and was removed.\n',i);
+    end
+end
+data = data(1:count,:);
+
+%{
+
 for i = 1:N
     %if "Temperature" condition is met, insert valid row in data. Else false element becomes 0.
     if (file(i, 1) >= 10) && (file(i, 1) <= 60)
@@ -41,4 +63,5 @@ for i = 1:N
         cond = data(:, j) == 0; %conditions for removing invalid rows, i.e. rows with zeros.
         data(cond, :) = []; %removes invalid rows where condition is met.
     end
+%}
 end
